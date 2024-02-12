ea7133fc82d5e3a7cab23fd30c58d27ed8b0ce7e
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2024-02-12 16:19:52 +0100

    20220613 tried js php

---
+++ 20220613/compito_a/jquery/soluzione.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    document.getElementsByTagName("form")[0].style.display = "none";
+    document.getElementsByTagName("span")[0].style.display = "none";
+    document.getElementsByTagName("button")[1].style.display = "none";
+}
